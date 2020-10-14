80d90641d6b23f18689539c88ec9527a7ced6f09
Jelle Glebbeek <jw.glebbeek@gmail.com>
2020-10-14 02:23:50 +0200

    Add basic GUI related methods

--- gui.py
+++ gui.py
@@ -1,42 +1,43 @@
 from tkinter import *
 from tkinter.ttk import *
-import threading
+import vragen
 
 
-class Interface(threading.Thread):
+class Interface:
     def __init__(self):
-        threading.Thread.__init__(self)
-        self.start()
-        self.progress_bar, self.progress_label, self.question, self.answer1, self.answer2, self.answer3, self.answer4, self.skip = (None,) * 8
-
-    def run(self):
+        self.current_question = None
+        self.current_answers = None
+        self.current_specialisation = None
         # Create a new window
-        window = Tk()
+        self.window = Tk()
         # Set the title of the new window
-        window.title("Sorteerhoed")
+        self.window.title("Sorteerhoed")
+        # Set fixed window size
+        self.window.geometry("600x400")
+        # Set window icon
+        self.window.iconbitmap("magichat.ico")
         # Remove the maximise button
-        window.resizable(0, 0)
+        self.window.resizable(0, 0)
 
         # Upper part of GUI (header / progess)
-        upper_frame = Frame(window)
+        upper_frame = Frame(self.window)
         can = Canvas(upper_frame, height=100, width=100)
         can.place(x=200, y=200, anchor=NW)
         header = Label(can, text="De magische sorteerhoed(TM)", font=("Arial Bold", 20))
         self.progress_label = Label(can, text="Vraag: x van y", font=("Arial Bold", 12))
-        print(self.progress_label)
         self.progress_bar = Progressbar(can, orient=HORIZONTAL, length=250, mode='determinate')
 
         # Frame seperator
-        separator = Separator(window)
+        separator = Separator(self.window)
 
         # Lower part of GUI
-        lower_frame = Frame(window)
-        question = Label(lower_frame, text="Is dit een vraag?", anchor='w', font=("Arial Bold", 12))
-        self.answer1 = Button(lower_frame, text="Antwoord1")
-        self.answer2 = Button(lower_frame, text="Antwoord2")
-        self.answer3 = Button(lower_frame, text="Antwoord3")
-        self.answer4 = Button(lower_frame, text="Antwoord4")
-        self.skip = Button(lower_frame, text="Sla over")
+        self.lower_frame = Frame(self.window)
+        self.question = Label(self.lower_frame, text="Is dit een vraag?", anchor='w', font=("Arial Bold", 12))
+        self.answer1 = Button(self.lower_frame, text="Antwoord1", command=lambda: self.submit_answer(self.answer1))
+        self.answer2 = Button(self.lower_frame, text="Antwoord2", command=lambda: self.submit_answer(self.answer2))
+        self.answer3 = Button(self.lower_frame, text="Antwoord3", command=lambda: self.submit_answer(self.answer3))
+        self.answer4 = Button(self.lower_frame, text="Antwoord4", command=lambda: self.submit_answer(self.answer4))
+        self.skip = Button(self.lower_frame, text="Sla over", command=lambda: self.load_question())
 
         # Pack all widgets in the right order
         header.pack(padx=10, pady=10)
@@ -45,19 +46,63 @@ class Interface(threading.Thread):
         can.pack()
         upper_frame.pack(side=TOP)
         separator.pack(pady=15, padx=35, fill='both')
-        question.pack(anchor=W, fill='both', pady=(5, 15))
+        self.question.pack(anchor=W, fill='both', pady=(5, 15), padx=35)
         self.answer1.pack(pady=5)
         self.answer2.pack(pady=5)
         self.answer3.pack(pady=5)
         self.answer4.pack(pady=5)
         self.skip.pack(pady=(5, 20))
-        lower_frame.pack(side=TOP)
+        self.lower_frame.pack(side=TOP)
+
+        # Show the first random question
+        self.load_question()
+
+        # Set the initial progress
+        self.set_progress()
 
         # Start the window loop
-        window.mainloop()
+        self.window.mainloop()
 
-    def set_progress(self, current, total):
+    def set_progress(self):
+        current = vragen.answered_questions
+        total = vragen.total_questions
         progress = current / total * 100
-        print(self.progress_label)
-        self.progress_label = f"Vraag: {current} van de {total}"
-        print(self.progress_label)
+        self.progress_label['text'] = f"Vraag: {current} van de {total}"
+        self.progress_bar['value'] = progress
+        self.progress_bar.update()
+
+    def load_question(self):
+        question_tuple = vragen.get_question()
+        if question_tuple is None:
+            print("Finished", vragen.points)
+            # TODO show score
+            return
+        question = question_tuple[0]
+        answers = question_tuple[1]
+        self.current_specialisation = question_tuple[2]
+        self.current_answers = answers
+        self.current_question = question
+        self.answer1['text'] = answers[0][0]
+        self.answer2['text'] = answers[1][0]
+        if len(answers) > 2:
+            self.answer3['text'] = answers[2][0]
+            if not self.answer3.winfo_ismapped():
+                self.answer3.pack(after=self.answer2, pady=5)
+        else:
+            self.answer3.pack_forget()
+        if len(answers) > 3:
+            self.answer4['text'] = answers[3][0]
+            if not self.answer4.winfo_ismapped():
+                self.answer4.pack(after=self.answer3, pady=5)
+        else:
+            self.answer4.pack_forget()
+        self.question['text'] = question
+
+    def submit_answer(self, button):
+        answer = button['text']
+        for answer_tuple in self.current_answers:
+            if answer_tuple[0] == answer:
+                vragen.question_answered(self.question['text'], answer_tuple[1], self.current_specialisation)
+                self.set_progress()
+                self.load_question()
+                return
